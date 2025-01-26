4aafca675c3f991fdc0709904908cfac1de2ce67
Axel Beaulieu <axel.b.luangkham@gmail.com>
2025-01-26 20:02:39 +0100

    tkt c'est mon commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,6 @@
-function change_theme() {
-    document.body.classList.toggle("dark-mode");
-    
-    if (document.body.classList.contains("dark-mode")) {
-        localStorage.setItem("theme", "sombre");
-        document.getElementById("theme-button").textContent = "Thème sombre";
-    } 
-    else {
-        localStorage.setItem("theme", "clair");
-        document.getElementById("theme-button").textContent = "Thème clair";
-    }
-}
+document.addEventListener('DOMContentLoaded', function() {
+    load_saved_theme();
+})
 
 function load_saved_theme() {
     const theme = localStorage.getItem("theme");
@@ -23,6 +14,41 @@ function load_saved_theme() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    load_saved_theme();
-});
+function change_theme() {
+    document.body.classList.toggle("dark-mode");
+    
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "sombre");
+        document.getElementById("theme-button").textContent = "Thème sombre";
+    } 
+    else {
+        localStorage.setItem("theme", "clair");
+        document.getElementById("theme-button").textContent = "Thème clair";
+    }
+}
+
+function changeUrl(){
+    const nomFilm = document.getElementById('oeuvre-name').value;
+    const lienImdb = document.getElementById('lienImdb');
+    const lienSenscritique = document.getElementById('lienSenscritique');
+    const lienAllocine = document.getElementById('lienAllocine');
+    const lienIgn = document.getElementById('lienIgn');
+
+    if (nomFilm) {
+        /* encode le nom du film en URL sécurisé */
+        const url_format_nomFilm = encodeURIComponent(nomFilm);
+
+        lienImdb.href = `https://www.imdb.com/find/?q=${url_format_nomFilm}&ref_=nv_sr_sm`;
+        lienImdb.textContent = `page ${nomFilm} sur IMDb`;
+
+        lienSenscritique.href = `https://www.senscritique.com/search?query=${url_format_nomFilm}`;
+        lienSenscritique.textContent = `page ${nomFilm} sur SensCritique`;
+
+        lienAllocine.href = `https://www.allocine.fr/rechercher/?q=${url_format_nomFilm}`;
+        lienAllocine.textContent = `page ${nomFilm} sur AlloCiné`;
+
+        lienIgn.href = `https://fr.ign.com/se/?model=&q=${url_format_nomFilm}`;
+        lienIgn.textContent = `page ${nomFilm} sur IGN`;
+
+    }
+};
